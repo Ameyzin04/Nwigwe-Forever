49b8952955468dee3067302dbb647159ddefc5c2
Software Engineer <AKsoftwareengineer@Ameyos-MacBook-Pro.local>
2024-04-02 12:26:47 -0400

    components celebration draft 1

---
+++ Nwigwe Wedding/src/Celebration.jsx
@@ -0,0 +1,20 @@
+export default function Celebration () {
+    return (
+        <div className="celebration">
+            <div className="weddingInfo">
+            <h1>Celebration</h1>
+            <h3>Saturday, December 20, 2025</h3>
+            <h2>Wedding Ceremony</h2>
+            <h2>2:30pm</h2>
+            <a href="https://maps.app.goo.gl/TGF6mxPxqdDL51DDA" className="Map"><button className= "see">Map</button></a>
+            </div>
+            <div className="receptionInfo">
+            <h3>Saturday, December 20, 2025</h3>
+            <h2>Reception</h2>
+            <h2>5:30pm</h2>
+            <a href="https://maps.app.goo.gl/pWMsu1SeTRHzLnB67" className="Map"><button className= "see">Map</button></a>
+
+            </div>
+        </div>
+    )
+}
