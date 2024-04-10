1e7b7dd4e8e5fb41f13340acfd4a3993bd5c4967
Software Engineer <AKsoftwareengineer@Ameyos-MacBook-Pro.local>
2024-04-09 21:56:02 -0400

    Celebration component complete

--- Nwigwe Wedding/src/Components/Celebration.jsx
+++ Nwigwe Wedding/src/Components/Celebration.jsx
@@ -1,3 +1,6 @@
+import '../styles/celebration.css'
+
+
 export default function Celebration () {
     return (
         <div className="celebration">
@@ -5,14 +8,15 @@ export default function Celebration () {
             <h1>Celebration</h1>
             <h3>Saturday, December 20, 2025</h3>
             <h2>Wedding Ceremony</h2>
+            <h2>Cathédrale Sacré-Coeur de Lomé</h2>
             <h2>2:30pm</h2>
-            <a href="https://maps.app.goo.gl/TGF6mxPxqdDL51DDA" className="Map"><button className= "see">Map</button></a>
+            <a href="https://maps.app.goo.gl/TGF6mxPxqdDL51DDA" className="Map"><button className= "see" id="button">Map</button></a>
             </div>
             <div className="receptionInfo">
-            <h3>Saturday, December 20, 2025</h3>
             <h2>Reception</h2>
+            <h2>Hôtel Mercure Sarakawa</h2>
             <h2>5:30pm</h2>
-            <a href="https://maps.app.goo.gl/pWMsu1SeTRHzLnB67" className="Map"><button className= "see">Map</button></a>
+            <a href="https://maps.app.goo.gl/pWMsu1SeTRHzLnB67" className="Map"><button className= "see" id="button">Map</button></a>
 
             </div>
         </div>
