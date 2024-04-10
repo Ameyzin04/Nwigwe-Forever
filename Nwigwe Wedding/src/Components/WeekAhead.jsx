d35d434314e5fb47ffb73f7fae22dff41d5b42e7
Software Engineer <AKsoftwareengineer@Ameyos-MacBook-Pro.local>
2024-04-09 21:25:09 -0400

    Week Ahead data + styling complete

--- Nwigwe Wedding/src/Components/WeekAhead.jsx
+++ Nwigwe Wedding/src/Components/WeekAhead.jsx
@@ -1,16 +1,40 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import '../styles/weekahead.css'
+
 export default function WeekAhead () {
+    let {eventsId} = useParams()
+    const [events , setEvents] = useState ([])
+
+
+    useEffect (() => {
+        const getEvents = async () => {
+            const response = await axios.get(`http://localhost:8000/api/events/`)
+            setEvents(response.data)
+        }
+
+
+        getEvents()
+    }, [eventsId]
+)
+
     return (
-        <div className="weekAheadr">
+        <div className="weekAhead">
             <h1> The Week Ahead</h1>
             <h2>Here is the schedule for all events happening the week of our weeding:</h2>
             <ul>
-                <li>
-            <h3>Thursday, December 18, 2025</h3>
-            <p>Social Soirée<br/>Come discover Lomé by Night</p>
-            
-            </li>
-
-            </ul>
-        </div>
+                {events.map(event => 
+                <li key={event.id}>
+                <h3>{event.name}</h3>
+                <p>{event.date}</p>
+                <p>{event.venue_name}</p>
+                </li>
+                )}
+                </ul>
+            </div>
+                
+                
     )
 }
